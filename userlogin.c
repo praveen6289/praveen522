86baf3327617916881048fd8ab08288b4b30abcc
praveen6289 <39346223+praveen6289@users.noreply.github.com>
2018-05-18 23:00:48 +0530

    Add files via upload

---
+++ userlogin.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+
+#include<stdlib.h>
+
+int
+main ()
+{
+  
+int username;
+  
+long int password;
+
+
+  
+printf ("please enter user name:");
+  
+scanf ("%d", &username);
+
+ 
+ printf ("please enter password:");
+  
+scanf ("%ld", &password);
+
+  
+if (password == 9848826598)
+    
+{
+
+      
+printf ("employee name: praveen\n");
+      
+printf ("employee ohrid:703199201\n");
+      
+printf ("employee process: Godaddy\n");
+      
+printf ("employee office:  Genpact\n");
+      
+printf ("employee location: Uppal\n");
+    
+}
+  
+else if (password == 9542421627)
+   
+ {
+      
+printf ("please reset password");
+  
+  }
+
+  
+else
+    
+{
+
+      
+printf ("worng password");
+      return 0;
+
+    }
